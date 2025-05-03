731218a60ff05d7ba28cd4e1b76775c802981649
seanwarrren <seanfwarr14@gmail.com>
2025-05-03 01:11:31 -0700

    update CorsUtils

--- final_project/src/main/java/com/budgetapp/Handlers/CorsUtils.java
+++ final_project/src/main/java/com/budgetapp/Handlers/CorsUtils.java
@@ -6,7 +6,16 @@ public class CorsUtils {
 
     // Sets required CORS headers to allow frontend to communicate with this server
     public static void setCorsHeaders(HttpExchange exchange) {
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+        String origin = exchange.getRequestHeaders().getFirst("Origin");
+
+        // Only allow trusted origins
+        if (origin != null && (
+            origin.equals("http://localhost:3000") || 
+            origin.equals("https://budgetapp-et01.onrender.com")
+        )) {
+            exchange.getResponseHeaders().set("Access-Control-Allow-Origin", origin);
+        }
+
         exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
     }
