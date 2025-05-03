8b78447b84f8d021d6d5846e95c236a5bd8ea39e
seanwarrren <seanfwarr14@gmail.com>
2025-05-03 03:05:15 -0700

    debug

--- final_project/src/main/java/com/budgetapp/Handlers/CorsUtils.java
+++ final_project/src/main/java/com/budgetapp/Handlers/CorsUtils.java
@@ -6,19 +6,19 @@ public class CorsUtils {
 
     // Sets required CORS headers to allow frontend to communicate with this server
     public static void setCorsHeaders(HttpExchange exchange) {
-        String origin = exchange.getRequestHeaders().getFirst("Origin");
-
-        // Only allow trusted origins
-        if (origin != null && (
-            origin.equals("http://localhost:3000") || 
-            origin.equals("https://budgetapp-et01.onrender.com")
-        )) {
-            exchange.getResponseHeaders().set("Access-Control-Allow-Origin", origin);
-        }
-
-        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Credentials", "true");
+        // hard-code your render front-end URL:
+        exchange.getResponseHeaders().set(
+          "Access-Control-Allow-Origin",
+          "https://budgetapp-et01.onrender.com"
+        );
+        exchange.getResponseHeaders().set(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PUT, DELETE, OPTIONS"
+        );
+        exchange.getResponseHeaders().set(
+          "Access-Control-Allow-Headers",
+          "Content-Type"
+        );
     }
 
     // Handles OPTIONS requests sent by the browser to check CORS permissions
